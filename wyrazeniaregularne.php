794d3a6e614cb51da38ae58cd010a29e84c3814c
2358 <2358@km-9550.mopr.lan>
2014-12-12 11:13:06 +0100

    regexp - preg_replace , preg_match

--- wyrazeniaregularne.php
+++ wyrazeniaregularne.php
@@ -9,7 +9,7 @@ preg_replace_callback
 preg_split
  */
 
-class regularexpress{
+class PregReplace{
     
     private $aReg = array();
 
@@ -66,50 +66,21 @@ class regularexpress{
     
 }
 
-$myregexpr = new regularexpress();
-$myregexpr->addArrayExpress('#http:\/\/(.*?)\.(.*?)\.(.*?)\/(.*?)\/(.*?)\/([0-9]?)#', '<br /><br />$0 <br /><br />$1 <br />$2 <br />$3 <br />$4 <br />$5 <br />$6 <br />$7', 'http://example.net.com/controller/action/3');
-$myregexpr->addArrayExpress('#<a href="(.*)" onclick="(.*)"> link (.*) ([0-9]+) ([0-9]*) ([a-z]ie.{1,2}) (.*)-([a-z]{3})</a>#', '<br /><br />$0 <br /><br />$1 <br />$2 <br />$3 <br />$4 <br />$5 <br />$6 <br />$7 <br />$8', '<a href="http://example.com/page/subpage/title" onclick="open(\'http://www.scratch24.com\'); return false;"> link do 434434 833 gierr gier-rrr</a>');
-$myregexpr->showResutls();
+$myPregReplace = new PregReplace();
+$myPregReplace->addArrayExpress('#http:\/\/(.*?)\.(.*?)\.(.*?)\/(.*?)\/(.*?)\/([0-9]?)#', '<br /><br />$0 <br /><br />$1 <br />$2 <br />$3 <br />$4 <br />$5 <br />$6 <br />$7', 'http://example.net.com/controller/action/3');
+$myPregReplace->addArrayExpress('#<a href="(.*)" onclick="(.*)"> link (.*) ([0-9]+) ([0-9]*) ([a-z]ie.{1,2}) (.*)-([a-z]{3})</a>#', '<br /><br />$0 <br /><br />$1 <br />$2 <br />$3 <br />$4 <br />$5 <br />$6 <br />$7 <br />$8', '<a href="http://example.com/page/subpage/title" onclick="open(\'http://www.scratch24.com\'); return false;"> link do 434434 833 gierr gier-rrr</a>');
+$myPregReplace->showResutls();
 
 
+class PregMatch {
+    function my_pregmatch($str) {
+        if(preg_match('/k..[^w]a/', $str, $matches )){
+            return $matches;
+        }else{
+            return 'Nie pasuje do wzorca';
+        }
+    }
+}
 
-
-//$toPregReplace = array(
-//'http://example.com' =>     array('/http:\/\/(.*?)\.(.*)/', '$1'),
-//'http://example.net.com' => array('/http:\/\/(.*?)\.(.*)/', '$1'),
-//'http://example.com/page/subpage/title' => array('/(.*)\/(.*?)/', '$2'),
-//'<a href="blablabal" onclick="OPEN(\'http://example.com\');return false">ascasc</a>' => array('/<a(.*)?onclick="(.*?)"(.*)/', '$2'),
-//'<a href="blablabal" onclick="OPEN(\'http://example.com\');return false">ascasc2</a>' => array('/<a(.*)?onclick="OPEN\((.*?)\)(.*?)"(.*)/', '$2'),
-//'<a href="blablabal" onclick="OPEN(\'http://example.com\');return false">ascasc3</a>' => array('/(.*)?onclick="(.*?)\'(http:\/\/.*?)\'(.*)"(.*)/', '$3'),
-//'text{link{alert}}text' => array('/(.*)?{(.*?){(.*)}}(.*)?/', 'onclick="alert($3);">$2'),
-//'01-blablabal-05_45_12.mp3' => array('/([0-9]*)(.*?)([0-9_]*)\.(.*)/', '$1_$3'),
-//'01_blablabal_05_45_12.mp3' => array('/([0-9]*)(.*?)(_*)?([0-9_]*)\.(.*)/', '$1_$4'),
-//'34573645/blablabal____05_45_12.mp3' => array('/([0-9]*)(.*?)(_*)?([0-9_]*)\.(.*)/', '$1_$4'),
-//'34573645/blablabal__|__05_45_12.mp3' => array('/([0-9]*)(.*?)(_*)?([0-9_]*)\.(.*)/', '$1_$4'),
-//
-//);
-//
-//echo '<table border="1">';
-//$i=0;
-//foreach($toPregReplace as $k => $v){
-//    echo '<tr>';
-//    echo '<td>'.++$i.'</td>';
-//    echo '<td>'.htmlspecialchars($k).'</td>';
-//    echo '<td>'.htmlspecialchars($v[0])."</td>";
-//    echo '<td>'.htmlspecialchars($v[1])."</td>";
-//    echo '<td>'.pr($v[0], $v[1], $k).'</td>';
-//    echo '<td><pre>';
-//   
-//    preg_match($v[0], $k, $matches);
-//    foreach($matches as $k2=>$v2){
-//        echo htmlspecialchars($k2)." => ".htmlspecialchars($v2)."<br>";
-//    }
-   
-   
-//    echo '</pre></td>';
-//    echo '</tr>';
-//}
-//
-//echo '</table>';
-//
-//
+$pregmatch = new PregMatch;
+print_r($pregmatch->my_pregmatch('kurka'));
