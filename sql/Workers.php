96caf89728582d05a939623a4c3ca00e9ce8b84e
admin <admin@km-9550.mopr.lan>
2014-12-15 12:25:45 +0100

    workers - sql

--- sql/Workers.php
+++ sql/Workers.php
@@ -1,15 +1,63 @@
 <?php
-var_dump(PDO::getAvailableDrivers());
+/*
+ * 
+Given the following data definition, select all queries that return a list of employees who are not managers.
+
+
+TABLE emp
+        id INTEGER PRIMARY KEY
+        mgrId INTEGER REFERENCES emp (id)
+        name VARCHAR2(30) NOT NULL
+
+SELECT e.name FROM emp e
+WHERE e.id NOT IN (
+        SELECT DISTINCT m.mgrId FROM emp m
+)
+
+SELECT e.name FROM emp e
+WHERE e.id NOT IN (
+        SELECT DISTINCT m.mgrId FROM emp m WHERE m.mgrId IS NOT NULL
+)
+
+SELECT e.name FROM emp e
+WHERE e.id NOT IN (
+        SELECT m.mgrId FROM emp m
+)
+
+SELECT e.name FROM emp e
+WHERE e.id NOT IN (
+        SELECT m.mgrId FROM emp m WHERE m.mgrId IS NOT NULL
+)
+   
+
+ */
 class connect{
-    private $dns = "mysql:host=locahost;port=3306;dbname=workers";
-    private $username = "worker";
-    private $password = "worker";
-    
+    private $dns = "mysql:host=localhost;port=3306;dbname=worker";
+    private $username = "root";
+    private $password = "";
+    private $pdo;
+            
     function __construct(){
         try{
-            $pdo = new PDO($dsn, $username, $passwd);
-        }catch(Exception $e){
+            $this->pdo = new PDO($this->dns, $this->username, $this->password);
+        }catch(PDOException $e){
             echo $e->getMessage();
         }
     }
+    function getData(){
+        $val = "SELECT e.name FROM emp e
+                    WHERE e.id NOT IN (
+                        SELECT DISTINCT m.mgrId FROM emp m WHERE m.mgrId IS NOT NULL)";
+        $val2 = "SELECT e.name FROM emp e
+                    WHERE e.id NOT IN (
+                        SELECT m.mgrId FROM emp m WHERE m.mgrId IS NOT NULL)";
+    
+        $query = $this->pdo->query($val);
+        foreach ($query as $row):
+            echo $row['name'].'<br />';
+        endforeach;
+    }
 }
+
+$pdo = new connect();
+$pdo->getData();
