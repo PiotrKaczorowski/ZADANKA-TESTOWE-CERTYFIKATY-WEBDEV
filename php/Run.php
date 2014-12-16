e344cc8e5febab42353634a8ab68aafde53f1e28
admin <admin@km-9550.mopr.lan>
2014-12-16 13:43:59 +0100

    run

--- php/Run.php
+++ php/Run.php
@@ -1,28 +1,32 @@
 <?php
-class Run
-{
 
-    public static function indexOfLongestRun($str)
-    {  
+class Run {
+
+    public static function indexOfLongestRun($str) {
         $aRes = array();
         $aStr = str_split($str);
-        $i=1;
+        $i = 1;
         foreach ($aStr as $key => $val) {
-            
-            if(($key!=0) && $aStr[$key-1] == $val) {
-                    $aRes[$val] = ++$i;
-            }else{
-                $i = 1;
-                $aRes[$val] = $i;
+
+            if (($key != 0) && ($aStr[$key - 1] == $val)) {
+                $aRes[$val] = ++$i;
+            }else {
+                if(!array_key_exists($val, $aRes)){
+                    $i = 1;
+                    $aRes[$val] = $i;
+                }else{
+                   
+                }
+                
             }
         }
         $aRes2 = array_flip($aRes);
-        
-        return strpos( $str , $aRes2[max($aRes)]);
+
+        return strpos($str, $aRes2[max($aRes)]);
     }
- 
+
 }
 
 // For testing purposes (do not submit uncommented):
 
-echo Run::indexOfLongestRun('aaaaabccccccccd');
+echo Run::indexOfLongestRun('aaaaabccccccdaaqqqq');
