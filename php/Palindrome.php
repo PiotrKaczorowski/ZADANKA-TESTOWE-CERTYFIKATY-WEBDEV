408472240ebc4403b89d967453509231036a82b5
admin <admin@km-9550.mopr.lan>
2014-12-16 11:36:15 +0100

    salary - sql

--- php/Palindrome.php
+++ php/Palindrome.php
@@ -5,18 +5,16 @@ class Palindrome {
     private static $a = array( 'Ę', 'Ó', 'Ą', 'Ś', 'Ł', 'Ż', 'Ź', 'Ć', 'Ń', 'ę', 'ó', 'ą','ś', 'ł', 'ż', 'ź', 'ć', 'ń' );
     private static $b = array( 'E', 'O', 'A', 'S', 'L', 'Z', 'Z', 'C', 'N', 'e', 'o', 'a','s', 'l', 'z', 'z', 'c', 'n' );
     
-    private function Parse( $string )
-	{
-		$string = str_replace( self::$a ,  self::$b, $string );
-                // If this modifier is set, letters in the pattern match both upper and lower case letters.
-                // $string = preg_replace( '#[^a-z0-9]#i', '', $string );
-		$string = preg_replace( '#[^A-Za-z0-9]#', '', $string ); 
-		// $string = preg_replace('#[\s]#', '' , $string);
-		$string = strtolower($string);
-		return $string;
-	}
-        
-        
+    private function Parse($string) {
+        $string = str_replace(self::$a, self::$b, $string);
+        // If this modifier is set, letters in the pattern match both upper and lower case letters.
+        // $string = preg_replace( '#[^a-z0-9]#i', '', $string );
+        $string = preg_replace('#[^A-Za-z0-9]#', '', $string);
+        // $string = preg_replace('#[\s]#', '' , $string);
+        $string = strtolower($string);
+        return $string;
+    }
+
     public static function isPalindrome($str) {
 
         $str = self::Parse($str);
--- php/Path.php
+++ php/Path.php
@@ -1,16 +1,119 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* ver 1
+ * 
+ * <?php
+
+  class Path {
+
+  public $currentPath;
+  private static $a = array('Ę', 'Ó', 'Ą', 'Ś', 'Ł', 'Ż', 'Ź', 'Ć', 'Ń', 'ę', 'ó', 'ą', 'ś', 'ł', 'ż', 'ź', 'ć', 'ń');
+  private static $b = array('E', 'O', 'A', 'S', 'L', 'Z', 'Z', 'C', 'N', 'e', 'o', 'a', 's', 'l', 'z', 'z', 'c', 'n');
+
+  function __construct($path) {
+  $this->currentPath = $this->Parse($path);
+
+  }
+
+  private function Parse($string) {
+  $string = str_replace(self::$a, self::$b, $string);
+  $string = preg_replace('#[^A-Za-z\/\.]#', '', $string);
+  $string = strtolower($string);
+  return $string;
+  }
+
+  public function cd($newPath) {
+  $newPath = $this->Parse($newPath);
+
+  if(substr($newPath, 0,1) ==='/'){
+  $this->currentPath = '/';
+  }else{
+  //if (strpos($newPath, '.') !== false) {
+  $aNewPath = explode('/', $newPath);
+  $valNewPath = count($aNewPath);
+  $replace = $aNewPath[$valNewPath - 1];
+
+  $aCurrentPath = explode('/', $this->currentPath);
+  $valCurrentPath = count($aCurrentPath);
+  $loops = $valCurrentPath - $valNewPath;
+
+  $this->currentPath = '';
+  for ($i = 1; $i <= $loops; $i++) {
+  $this->currentPath .= '/' . $aCurrentPath[$i];
+  }
+  $this->currentPath .= '/' . $aNewPath[$valNewPath - 1];
+  //}
+  }
+  return $this;
+
+  }
+
+  }
+
+  // For testing purposes (do not submit uncommented):
+
+  $path = new Path('/a/b/c/d');
+  //echo $path->cd('../../x')->currentPath;
+  //echo '<br />'.$path->cd('../y')->currentPath;
+  var_dump($path->cd('../x')->currentPath);
+
+
  */
 
-/**
- * Description of path
- *
- * @author admin
- */
 class Path {
-    //put your code here
+
+    public $currentPath;
+    private static $a = array('Ę', 'Ó', 'Ą', 'Ś', 'Ł', 'Ż', 'Ź', 'Ć', 'Ń', 'ę', 'ó', 'ą', 'ś', 'ł', 'ż', 'ź', 'ć', 'ń');
+    private static $b = array('E', 'O', 'A', 'S', 'L', 'Z', 'Z', 'C', 'N', 'e', 'o', 'a', 's', 'l', 'z', 'z', 'c', 'n');
+
+    function __construct($path) {
+        $this->currentPath = $this->Parse($path);
+    }
+
+    private function Parse($string) {
+        $string = str_replace(self::$a, self::$b, $string);
+        $string = preg_replace('#[^A-Za-z\/\.]#', '', $string);
+
+        $string = strtolower($string);
+        return $string;
+    }
+
+    public function cd($newPath) {
+        $newPath = $this->Parse($newPath);
+        $aNewPath = explode('/', $newPath);
+        $aCurrentPath = explode('/', $this->currentPath);
+        $this->currentPath = '';
+        $ile_w_dol = substr_count($newPath, '../') + 1;
+      
+        if (substr($newPath, 0,2) === './') {
+            return false;
+        }
+        if(preg_match_all('#[^\.]\.\/#', $newPath)){
+            return false;
+        } 
+
+        for ($i = 1; $i <= count($aCurrentPath) - $ile_w_dol; $i++) {
+            $this->currentPath .= '/' . $aCurrentPath[$i];
+        }
+        $newPathClear = str_replace('../', '', $newPath);
+        $this->currentPath .= '/' . $newPathClear;
+
+        if (substr($newPath, 0, 1) === '/') {
+
+            if(substr($newPath, 1, 1)==='.'){
+                return false;
+            }
+            $this->currentPath = $newPath;
+        }
+
+        return $this;
+    }
+
 }
+
+// For testing purposes (do not submit uncommented):
+
+$path = new Path('/a/b/c/d');
+//echo $path->cd('../../x')->currentPath;
+//echo '<br />'.$path->cd('../y')->currentPath;
+echo $path->cd('../das/../fsdf')->currentPath;
