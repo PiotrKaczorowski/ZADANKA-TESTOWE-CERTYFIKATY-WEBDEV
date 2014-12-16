1e169c06133e390115035f3b7a75bcee1c73731b
admin <admin@km-9550.mopr.lan>
2014-12-16 13:07:12 +0100

    run

--- php/Run.php
+++ php/Run.php
@@ -4,35 +4,27 @@ class Run
 
     public static function indexOfLongestRun($str)
     {  
-
+        $aRes = array();
         $aStr = str_split($str);
         $i=1;
         foreach ($aStr as $key => $val) {
             
-            if($key!=0 && ($aStr[$key-1] == $val)) {
-                
-//                if(count($aResult[$val])){
-//                    $aResult2[$val] = ++$i;
-//                    if(count($aResult2[$val])>count($aResult2[$val])){
-//                        $aResult = $aResult2;
-//                    }
-               // }else{
-                $all = array( 
-                    $key => array($aResult[$val] => ++$i)
-                );
-               //    }
-                
+            if(($key!=0) && $aStr[$key-1] == $val) {
+     //           if(!isset($aRes[$val])){
+                    $aRes[$val] = ++$i;
+      //          }
             }else{
                 $i = 1;
+                $aRes[$val] = $i;
             }
            
             
         }
-        return $all;
+        return $aRes;
         //throw new Exception('Not implemented');
     }
 }
 
 // For testing purposes (do not submit uncommented):
 
-print_r(Run::indexOfLongestRun('aaaaaaccccccccaabbccccddddccccccbbaaa'));
+print_r(Run::indexOfLongestRun('aaaaabcccccd'));
