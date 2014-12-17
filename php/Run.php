e879b0a047276a3b885e6feaffd135776d7523bd
2358 <2358@km-9550.mopr.lan>
2014-12-17 13:11:58 +0100

    run

--- php/Run.php
+++ php/Run.php
@@ -5,32 +5,22 @@ class Run {
     public static function indexOfLongestRun($str) {
         $aRes = array();
         $aStr = str_split($str);
-        $i = 1;
         foreach ($aStr as $key => $val) {
-
-            if (($key != 0) && ($aStr[$key - 1] == $val)) {
-                $aRes[$val] = ++$i;
+            if ($aStr[$key + 1] == $val) {
+                $string .= $val; 
             } else {
-                // czy $val jest już w $aqRes
-                if(array_key_exists($val, $aRes)){
-                                       
- //                   $aRes[$val]>
-                   //$gdzie = array_search($val, $aStr);
-                }else{
-                    $i = 1;
-                    $aRes[$val] = $i;
-                }
-                    
-                
+                $string .= $val.'/'; 
             }
         }
-        $aRes2 = array_flip($aRes);
-
-        return strpos($str, $aRes2[max($aRes)]);
+        $string = substr($string, 0 , -1);
+        $aRes = explode('/' , $string);
+        //$aRes2 = array_flip($aRes);
+        print_r($aRes);
+        //return strpos($str, $aRes2[max($aRes)]);
     }
 
 }
 
 // For testing purposes (do not submit uncommented):
 
-echo Run::indexOfLongestRun('aaaaabccccccdaaqqqq');
+echo Run::indexOfLongestRun('aaaaabccccccdaaqqqqbbx');
