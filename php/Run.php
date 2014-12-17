2f93b02d6b752ed2755696073909057fa71562e4
2358 <2358@km-9550.mopr.lan>
2014-12-17 14:32:47 +0100

    run

--- php/Run.php
+++ php/Run.php
@@ -4,9 +4,11 @@ class Run {
 
     public static function indexOfLongestRun($str) {
         $aRes = array();
+        $string = '';
+        
         $aStr = str_split($str);
         foreach ($aStr as $key => $val) {
-            if ($aStr[$key + 1] == $val) {
+            if ((count($aStr)>$key+1) && ($aStr[$key+1] === $val)) {
                 $string .= $val; 
             } else {
                 $string .= $val.'/'; 
@@ -15,12 +17,22 @@ class Run {
         $string = substr($string, 0 , -1);
         $aRes = explode('/' , $string);
         //$aRes2 = array_flip($aRes);
-        print_r($aRes);
-        //return strpos($str, $aRes2[max($aRes)]);
+        foreach($aRes as $key => $val){
+            $tab[$key] = strlen($val);
+        }
+        //return $aStr;
+        $max = max($tab);
+        $ile = count($tab);
+        while(--$ile){
+            if($tab[$ile] == $max){
+                $ok = $ile;
+            }
+        }
+        return $aRes[$ok];
     }
 
 }
 
 // For testing purposes (do not submit uncommented):
 
-echo Run::indexOfLongestRun('aaaaabccccccdaaqqqqbbx');
+echo Run::indexOfLongestRun('aaaaaaaaaaabcccccdaaaaaaaaaaaaaqqqqbbx');
