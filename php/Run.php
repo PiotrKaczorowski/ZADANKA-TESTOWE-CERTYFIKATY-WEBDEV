2f549e2fb445229bdfbe3bc536e91a6b6fc4c7af
2358 <2358@km-9550.mopr.lan>
2014-12-17 14:50:06 +0100

    run

--- php/Run.php
+++ php/Run.php
@@ -3,9 +3,7 @@
 class Run {
 
     public static function indexOfLongestRun($str) {
-        $aRes = array();
-        $string = '';
-        
+        $string = '';      
         $aStr = str_split($str);
         foreach ($aStr as $key => $val) {
             if ((count($aStr)>$key+1) && ($aStr[$key+1] === $val)) {
@@ -14,25 +12,23 @@ class Run {
                 $string .= $val.'/'; 
             }
         }
-        $string = substr($string, 0 , -1);
-        $aRes = explode('/' , $string);
-        //$aRes2 = array_flip($aRes);
+        $aRes = explode('/' , substr($string, 0 , -1));
+
         foreach($aRes as $key => $val){
             $tab[$key] = strlen($val);
         }
-        //return $aStr;
-        $max = max($tab);
-        $ile = count($tab);
-        while(--$ile){
-            if($tab[$ile] == $max){
-                $ok = $ile;
-            }
-        }
-        return strpos($str , $aRes[$ok]);
+        
+//        $max = max($tab);
+//        $ile = count($tab);
+//        while(--$ile){
+//            if($tab[$ile] == $max){
+//                $ok = $ile;
+//            }
+//        }
+        $ok = array_keys($tab , max($tab));
+        return strpos($str , $aRes[$ok[0]]);
     }
 
 }
 
-// For testing purposes (do not submit uncommented):
-
-echo Run::indexOfLongestRun('aaaaabbbbbbbbbbbbcccccdaaaaaaaqqqqbbx');
+echo Run::indexOfLongestRun('aaaaabbbbbbbbbbbbcccccdaaaaaaaqqqqbbxa');
