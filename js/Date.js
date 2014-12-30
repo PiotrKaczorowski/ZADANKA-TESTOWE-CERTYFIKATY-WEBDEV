e9739e5beb64cfaf1cf5ea516267fc6c8798235f
2358 <2358@km-9550.mopr.lan>
2014-12-30 12:44:06 +0100

    PHP , SQL , HTML , JS 100%

--- js/Date.js
+++ js/Date.js
@@ -1,8 +1,24 @@
 /*
+ * 100%
  *
 Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
 For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
- */
+
 function formatDate(userDate) {
    // format from M/D/YYYY to YYYYMMDD
 }
+*
+*/
+function formatDate(userDate) {
+    var aMy = userDate.split("/");
+   
+    if(aMy[0]<=9){
+        aMy[0] = '0' + aMy[0];
+    }
+     if(aMy[1]<=9){
+        aMy[1] = '0' + aMy[1];
+    }
+    
+    return aMy[2]+aMy[0]+aMy[1];
+}
+alert(formatDate('2/1/2014'));
