326a456e5f366e811596f77c4468373897d5dc06
2358 <2358@km-9550.mopr.lan>
2015-01-07 15:28:17 +0100

    loop.js

--- js/loop.js
+++ js/loop.js
@@ -1,14 +1,17 @@
 function appendChildren() {
+   
   var allDivs = document.getElementsByTagName("div");
-
-  for (var i = 0; i < allDivs.length; i++) {
+  for (i = 0; i < allDivs.length; i++) {
     var newDiv = document.createElement("div");
-   // decorateDiv(newDiv);
-    allDivs[i].appendChild(newDiv);
+    decorateDiv(newDiv);
+    // allDivs[i].appendChild(newDiv);
   }
 }
-
+$('document').ready(function(){
+    appendChildren();
+});
 // Mock of decorateDiv function for testing purposes
-function decorateDiv(div) {}
-
+function decorateDiv(div) {
+    
+}
 
