1dd8f0beef92709ef00757c8875384c1ff8d252c
2358 <2358@km-9550.mopr.lan>
2015-01-07 13:35:55 +0100

    Closures.js

--- js/Closures.js
+++ js/Closures.js
@@ -1,6 +1,6 @@
 /* 
  * 
-function registerHandlers() {
+ function registerHandlers() {
   var as = document.getElementsByTagName('a');
   for (i = as.length; i-- >= 0;) {
     as[i].onclick = function() {
@@ -11,19 +11,42 @@ function registerHandlers() {
 }
  * 
  */
-//function registerHandlers() {
-$(function(){
-    var as = $('a');
-    console.log(as);
+
+$('document').ready(function(){
+  registerHandlers();  
 });
+
+
+function registerHandlers() {
+  var as = document.getElementsByTagName('a');
+  var tab = new Array();
+  var i;
   
-//  for (i = as.length; i-- >= 0;) {
-//    as[i].onclick = function() {
-//      alert(i);
-//      return false;
+  
+  for (i = as.length-1; i >= 0; i--) {
+    tab[i] = i;  
+    (function(e) {
+        as[i].onclick = function() {
+          alert(tab[e]);
+          return false;
+        }
+    })(i);
+   
+  }
+}
+
+
+//function celebrityName(firstName) {
+//    var nameIntro = "This celebrity is ";
+//    // this inner function has access to the outer function's variables, including the parameter​
+//    function lastName(theLastName) {
+//        return nameIntro + firstName + " " + theLastName;
 //    }
-//  }
+//    return lastName;
 //}
-
-
-
+//
+//
+//wynik = celebrityName("Nelson");
+//console.log(wynik("Jackson")); // This celebrity is Michael Jackson 
+//console.log(wynik("Kaczorowski"));
+//console.log(wynik("Kowalski"));
