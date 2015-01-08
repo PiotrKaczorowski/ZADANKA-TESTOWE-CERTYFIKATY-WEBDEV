88c677f697b50cafe9049be5780ea2b8e9d4cff1
2358 <2358@km-9550.mopr.lan>
2015-01-08 11:31:33 +0100

    loop.js

--- js/loop.js
+++ js/loop.js
@@ -1,17 +1,14 @@
-function appendChildren() {
-   
+function appendChildren() { 
   var allDivs = document.getElementsByTagName("div");
   for (i = 0; i < allDivs.length; i++) {
-    var newDiv = document.createElement("div");
-    decorateDiv(newDiv);
-    // allDivs[i].appendChild(newDiv);
+    decorateDiv(allDivs[i]);  
   }
+}
+// Mock of decorateDiv function for testing purposes
+function decorateDiv(allDiv) {
+    var newDiv = document.createElement("div");
+    allDiv.appendChild(newDiv);
 }
 $('document').ready(function(){
     appendChildren();
 });
-// Mock of decorateDiv function for testing purposes
-function decorateDiv(div) {
-    
-}
-
