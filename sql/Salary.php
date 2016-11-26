0e0a1a85051ee8afe6a9b06567f39d59ff329e1e
Brzeźno Piotr Kaczorowski <office@ctechnology.pl>
2016-11-26 13:48:02 +0100

    Reflection

--- sql/Salary.php
+++ sql/Salary.php
@@ -6,16 +6,22 @@ Question: Salary
 
 Time: 5min Skills: SQL  
 Type: PUBLIC
-Given the following data definition, select all queries that return the second largest salary. Note that it is possible that some employees have the same salary.
+Given the following data definition, select all queries that return the second 
+largest salary. Note that it is possible that some employees have the same salary.
 
 TABLE emp
         id INTEGER PRIMARY KEY
-        name VARCHAR2(30) NOT NULL
-        salary NUMBER
+        name VARCHAR(30) NOT NULL
+        salary Numeric
 
-If we have employees A, B, and C whose salaries are $100, $80, and $100 respectively, note that the second highest salary is $80 although there are two employees with a higher salary.
 
-SELECT DISTINCT salary FROM emp ORDER BY salary DESC OFFSET 1 LIMIT 1;
+    SELECT DISTINCT salary FROM emp WHERE 1=1 ORDER BY salary ASC LIMIT 1
+
+
+If we have employees A, B, and C whose salaries are $100, $80, and $100 respectively,
+note that the second highest salary is $80 although there are two employees with a higher salary.
+
+SELECT DISTINCT salary FROM emp ORDER BY salary DESC LIMIT 1 OFFSET 1
 SELECT MAX(salary) FROM emp WHERE salary < (SELECT MAX(salary) FROM emp);
 SELECT salary FROM (SELECT DISTINCT salary FROM emp ORDER BY salary DESC LIMIT 2) AS emp ORDER BY salary LIMIT 1;
 SELECT DISTINCT salary FROM (SELECT salary FROM emp ORDER BY salary DESC LIMIT 2) AS emp ORDER BY salary LIMIT 1;
@@ -26,6 +32,7 @@ SELECT salary FROM emp ORDER BY salary DESC OFFSET 1 LIMIT 1;
  */
 class Salary{
     private $dns = "mysql:host=localhost;port=3306;dbname=worker";
+    
     private $username = "worker";
     private $password = "worker";
     private $pdo;
