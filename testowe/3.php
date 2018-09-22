843178737fdf90d1a2b21682dccb8399a00d8c3d
Piotr <Piotr@192.168.0.80>
2018-09-22 17:03:42 +0200

    Palindrome new wersion

--- testowe/3.php
+++ testowe/3.php
@@ -5,13 +5,25 @@ class Palindrome
        $str = preg_replace('#[^A-Za-z0-9]#' , '' , trim(strtolower($str)));
        return $str; 
     }
-    public static function isPalindrome($str) {
+//  public static function isPalindrome($str)
+//  {
+//        $str = self::convertString($str);
+//        $aStr = str_split($str);
+//        $aRevStr = array_reverse($aStr);
+//        $aDiffs = array_diff_assoc($aStr, $aRevStr); 
+//
+//        if(!count($aDiffs)){
+//            return false;
+//        }
+//        
+//        return true;
+//  }
+    public static function isPalindrome2018($str) {
          $str = self::convertString($str);
          return($str === strrev($str)) ? true : false;
         
     }
 }
-// For testing purposes (do not submit uncommented):
-var_dump(Palindrome::isPalindrome('Noel sees L?eon.'));
+var_dump(Palindrome::isPalindrome2018('Noel sees L?eon.'));
 
 
